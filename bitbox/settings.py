643814ddbb3f02f0fb8f0d1398b053dec63d709a
justshivam <stdnt.shivam@gmail.com>
2021-03-28 00:19:10 +0530

    Update settings.py

--- bitbox/settings.py
+++ bitbox/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^677la_bv4(sn%y)l-3_q-k$c22c11fasyl-5=p9#bc_4uuchp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
